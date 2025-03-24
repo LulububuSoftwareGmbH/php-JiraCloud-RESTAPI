546caeb82235ed4648c761d6ef36e40ca9eb441a
Philippos Tiropoulos <philippos@lulububu.de>
2025-03-24 18:54:35 +0100

    Added getter functions and setter functions for JsonMapper (https://lulububu.atlassian.net/browse/ROBOTHOMAS-1017)

--- src/Issue/Issue.php
+++ src/Issue/Issue.php
@@ -7,33 +7,163 @@ class Issue implements \JsonSerializable
     /**
      * return only if Project query by key(not id).
      */
-    public ?string $expand;
+    protected ?string $expand;
 
-    public string $self;
+    protected string $self;
 
-    public string $id;
+    protected string $id;
 
-    public string $key;
+    protected string $key;
 
-    public IssueField $fields;
+    protected IssueField $fields;
 
-    public ?array $renderedFields;
+    protected ?array $renderedFields;
 
-    public ?array $names;
+    protected ?array $names;
 
-    public ?array $schema;
+    protected ?array $schema;
 
-    public ?array $transitions;
+    protected ?array $transitions;
 
-    public ?array $operations;
+    protected ?array $operations;
 
-    public ?array $editmeta;
+    protected ?array $editmeta;
 
-    public ?ChangeLog $changelog;
+    protected ?ChangeLog $changelog;
 
     #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return array_filter(get_object_vars($this));
+    }
+
+    public function getFields(): ?IssueField
+    {
+        return $this->fields;
+    }
+
+    public function getStatus(): IssueStatus
+    {
+        return $this->fields->status;
+    }
+
+    public function getStatusId(): string
+    {
+        return $this->fields->status->id;
+    }
+
+    public function getExpand(): ?string
+    {
+        return $this->expand;
+    }
+
+    public function getSelf(): string
+    {
+        return $this->self;
+    }
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    public function getRenderedFields(): ?array
+    {
+        return $this->renderedFields;
+    }
+
+    public function getNames(): ?array
+    {
+        return $this->names;
+    }
+
+    public function getSchema(): ?array
+    {
+        return $this->schema;
+    }
+
+    public function getTransitions(): ?array
+    {
+        return $this->transitions;
+    }
+
+    public function getOperations(): ?array
+    {
+        return $this->operations;
+    }
+
+    public function getEditmeta(): ?array
+    {
+        return $this->editmeta;
+    }
+
+    public function getChangelog(): ?ChangeLog
+    {
+        return $this->changelog;
+    }
+
+    public function setExpand(?string $expand): void
+    {
+        $this->expand = $expand;
+    }
+
+    public function setSelf(string $self): void
+    {
+        $this->self = $self;
+    }
+
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setKey(string $key): void
+    {
+        $this->key = $key;
+    }
+
+    public function setFields(IssueField $fields): void
+    {
+        $this->fields = $fields;
+    }
+
+    public function setRenderedFields(?array $renderedFields): void
+    {
+        $this->renderedFields = $renderedFields;
+    }
+
+    public function setNames(?array $names): void
+    {
+        $this->names = $names;
+    }
+
+    public function setSchema(?array $schema): void
+    {
+        $this->schema = $schema;
+    }
+
+    public function setTransitions(?array $transitions): void
+    {
+        $this->transitions = $transitions;
+    }
+
+    public function setOperations(?array $operations): void
+    {
+        $this->operations = $operations;
+    }
+
+    public function setEditmeta(?array $editmeta): void
+    {
+        $this->editmeta = $editmeta;
+    }
+
+    public function setChangelog(?ChangeLog $changelog): void
+    {
+        $this->changelog = $changelog;
     }
 }
