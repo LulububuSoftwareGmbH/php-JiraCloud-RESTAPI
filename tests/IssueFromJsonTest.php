4baf8a75bfeeb35cb8958d0639d882124b17aa20
Philippos Tiropoulos <philippos@lulububu.de>
2025-03-25 10:15:34 +0100

    Removed copyright header (https://lulububu.atlassian.net/browse/ROBOTHOMAS-1015)

--- tests/IssueFromJsonTest.php
+++ tests/IssueFromJsonTest.php
@@ -1,16 +1,5 @@
 <?php
 
-//  _        _      _         _
-// | |  _  _| |_  _| |__ _  _| |__ _  _
-// | |_| || | | || | '_ \ || | '_ \ || |
-// |____\_,_|_|\_,_|_.__/\_,_|_.__/\_,_|
-//
-// Copyright © Lulububu Software GmbH - All Rights Reserved
-// https://lulububu.de
-//
-// Unauthorized copying of this file, via any medium is strictly prohibited!
-// Proprietary and confidential.
-
 namespace JiraCloud\Test;
 
 use JiraCloud\Issue\Issue;
